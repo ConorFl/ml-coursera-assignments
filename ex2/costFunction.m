7114226b53a5a34c2e4ba9f688e05abffcd80b71
ConorFl <conoreflanagan@gmail.com>
2016-04-10 13:02:04 -0700

    update to the costFn + predict function (with rounding) + add some semi-colons

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -23,9 +23,14 @@ grad = zeros(size(theta));
 
 
 J = 1 / m * sum(-1 * y .* log(sigmoid(X * theta)) -
-				(1 - y) .* log(1 - sigmoid(X * theta)))
+				(1 - y) .* log(1 - sigmoid(X * theta)));
 
+% Full answer for n x 1 vector: sum(sigmoid(X * theta - y) .* X).'
+diffs = sigmoid(X * theta) - y; % n x 1 vector
+mult_across_x = diffs .* X; 	% m x n matrix where each each element was mult by the row in diffs vector
+sum_down = sum(mult_across_x);	% 1 x n row vector of all sums for each j in [1, n]
 
+grad = 1 / m .* (sum_down.');	% Transpose sum_down to an n x 1 vector (this step seems optional)
 
 % =============================================================
 
