8b2cdc82decbfa8a9427edeb6ce260e31ba924c8
ConorFl <conoreflanagan@gmail.com>
2016-05-27 15:54:05 -0700

    implement finding closest centroid

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,11 +20,24 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
+X
+centroids
 
+for i = 1:size(X,1)
+	% Get row vector of element x_i
+	x = X(i, :);
+	% Subtract x from ALL centroids (Octave magic)
+	distances = centroids - x;
+	% Square values
+	distances = distances .^ 2;
+	% then sum for distance
+	distancesV = sum(distances, 2);
+	% Find min distance (technically a sq root should be used for distance 
+	% but it doesn't matter since we're find the min)
+	[val, index] = min(distancesV);
+	idx(i) = index;
 
-
-
-
+end
 
 
 % =============================================================
